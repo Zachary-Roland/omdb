f8e1d5d05eac8da6e9ab109b3c2424d4f7d851bb
Zachary-Roland <zsolo95@gmail.com>
2021-06-25 16:51:11 -0500

    removes comment

--- src/components/ResultsContainer.js
+++ src/components/ResultsContainer.js
@@ -5,13 +5,6 @@ const ResultsContainer = ({ response }) => {
   let totalResults = response.totalResults;
   const isResponse = response.Response;
   let searchResults = response.Search;
-  //   console.log(searchResults);
-  //   if (isResponse === "False") {
-  //     totalResults = "No";
-  //   } else {
-  //     console.log(isResponse);
-  //     console.log(totalResults);
-  //   }
   // !  You can write conditionals for rendering elements such as one of these options:
   //! {someBoolean ? <div>Is True</div> : <div> Is False</div>}
   //! {someBoolean && <h1>Is True</div>}
