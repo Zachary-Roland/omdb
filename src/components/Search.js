f12f44563450f2c9f1ef7cae55695da9bc0b6b63
Zachary-Roland <zsolo95@gmail.com>
2021-06-25 12:25:08 -0500

    adds year option dropdown

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,18 @@
+import { render } from "@testing-library/react";
 import React, { useState } from "react";
 
 const Search = () => {
-  const [search, setSearch] = (useState = "");
+  const [search, setSearch] = useState("");
+  const years = [];
+  for (let i = 1908; i <= new Date().getFullYear(); i++) {
+    years.push(i);
+  }
   return (
-    <body>
+    <div>
       <header>Header</header>
       <aside>
         <form>
+          {/* search input */}
           <label htmlFor="search">Search:</label>
           <input
             id="search"
@@ -17,9 +23,50 @@ const Search = () => {
               console.log(search);
             }}
           ></input>
+          {/* year dropdown - use a map function to render years from 1908 to present */}
+          <div>
+            <label htmlFor="yearDropdown">Year:</label>
+            <select name="yearDropdown" id="years">
+              {years.map((y) => (
+                <option key={y} value={y}>
+                  {y}
+                </option>
+              ))}
+            </select>
+          </div>
+          {/* type radios */}
+          <div>
+            <input 
+            type="radio" 
+            name="type" 
+            id="typeBoth"
+            value=""
+            checked={selectedOption = ""}
+            onChange={}
+            ></input>
+            <label htmlFor="typeBoth">Both</label>
+            <input 
+            type="radio" 
+            name="type" 
+            id="typeMovie"
+            value="movie"
+            checked={selectedOption = "movie"}
+            onChange={}
+            ></input>
+            <label htmlFor="typeMovie">Movie</label>
+            <input 
+            type="radio" 
+            name="type" 
+            id="typeSeries"
+            value="series"
+            checked={selectedOption = "series"}
+            onChange={}
+            ></input>
+            <label htmlFor="typeSeries">Series</label>
+          </div>
         </form>
       </aside>
-    </body>
+    </div>
   );
 };
 
