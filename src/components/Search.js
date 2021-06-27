f5b6c44444fc2c8078c4c3548b16bf54a28189ee
Zachary-Roland <zsolo95@gmail.com>
2021-06-27 18:08:42 -0500

    adds css to search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ResultsContainer from "./ResultsContainer";
+import "./Search.css";
 
 const Search = () => {
   const [search, setSearch] = useState("");
@@ -16,111 +17,122 @@ const Search = () => {
   }
   // TODO Search is functional, later on add pagination using 'page' API key?
   return (
-    <>
-      <header>OMDb Search!</header>
-      <aside>
-        <form>
-          {/* search input */}
-          <div>
-            <label htmlFor="search">Search:</label>
-            <input
-              id="search"
-              value={search}
-              placeholder="Movie Search"
-              onChange={(e) => {
-                setSearch(e.target.value);
-              }}
-            ></input>
-          </div>
-          {/* year dropdown - use a map function to render years from 1908 to present */}
-          <div>
-            <label htmlFor="yearDropdown">Year:</label>
-            <select
-              name="yearDropdown"
-              id="years"
-              value={searchYear}
-              onChange={(e) => {
-                setSearchYear(e.target.value);
+    <div className="textCenter">
+      <header className="font800">OMDb Search!</header>
+      <aside className="flex aside textCenter bgSecondary flex100">
+        <div className="searchItems adaptHeight flex100 bgSecondary">
+          <form className="form flex">
+            {/* search input */}
+            <div className="flex100 lineHeight50">
+              <label htmlFor="search">Search: </label>
+              <input
+                id="search"
+                value={search}
+                placeholder="Movie Search"
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+              ></input>
+            </div>
+            {/* year dropdown - use a map function to render years from 1908 to present */}
+            <div className="flex100 lineHeight50">
+              <label htmlFor="yearDropdown">Year: </label>
+              <select
+                className="bgPrimary"
+                name="yearDropdown"
+                id="years"
+                value={searchYear}
+                onChange={(e) => {
+                  setSearchYear(e.target.value);
+                }}
+              >
+                <option key="any" value="any">
+                  Any
+                </option>
+                {years.map((y) => (
+                  <option key={y} value={y}>
+                    {y}
+                  </option>
+                ))}
+              </select>
+            </div>
+            {/* type radios */}
+            <div className="flex100  lineHeight50 flex jcSpaceAround">
+              <div className="radioOption">
+                <input
+                  type="radio"
+                  name="type"
+                  id="typeBoth"
+                  value=""
+                  checked={searchType === ""}
+                  onChange={(e) => {
+                    setSearchType(e.target.value);
+                    console.log(searchType);
+                  }}
+                ></input>
+                <label htmlFor="typeBoth">Both</label>
+              </div>
+              <div className="radioOption">
+                <input
+                  type="radio"
+                  name="type"
+                  id="typeMovie"
+                  value="movie"
+                  checked={searchType === "movie"}
+                  onChange={(e) => {
+                    setSearchType(e.target.value);
+                    console.log(searchType);
+                  }}
+                ></input>
+                <label htmlFor="typeMovie">Movie</label>
+              </div>
+              <div className="radioOption">
+                <input
+                  type="radio"
+                  name="type"
+                  id="typeSeries"
+                  value="series"
+                  checked={searchType === "series"}
+                  onChange={(e) => {
+                    setSearchType(e.target.value);
+                    console.log(searchType);
+                  }}
+                ></input>
+                <label htmlFor="typeSeries">Series</label>
+              </div>
+            </div>
+          </form>
+          <div className="flex100">
+            <button
+              className="font600"
+              id="submit"
+              onClick={(e) => {
+                // fetch API URL plus s={search} plus y={year} plus type={type}
+                fetch(
+                  "http://www.omdbapi.com/?apikey=5d6248fb&s=" +
+                    search +
+                    "&y=" +
+                    searchYear +
+                    "&type=" +
+                    searchType
+                )
+                  .then((res) => {
+                    //   console.log(res);
+                    return res.json();
+                  })
+                  .then((res) => {
+                    //   console.log(res);
+                    setResponse(res);
+                  });
               }}
             >
-              <option key="any" value="any">
-                Any
-              </option>
-              {years.map((y) => (
-                <option key={y} value={y}>
-                  {y}
-                </option>
-              ))}
-            </select>
+              Search!
+            </button>
           </div>
-          {/* type radios */}
-          <div>
-            <input
-              type="radio"
-              name="type"
-              id="typeBoth"
-              value=""
-              checked={searchType === ""}
-              onChange={(e) => {
-                setSearchType(e.target.value);
-                console.log(searchType);
-              }}
-            ></input>
-            <label htmlFor="typeBoth">Both</label>
-            <input
-              type="radio"
-              name="type"
-              id="typeMovie"
-              value="movie"
-              checked={searchType === "movie"}
-              onChange={(e) => {
-                setSearchType(e.target.value);
-                console.log(searchType);
-              }}
-            ></input>
-            <label htmlFor="typeMovie">Movie</label>
-            <input
-              type="radio"
-              name="type"
-              id="typeSeries"
-              value="series"
-              checked={searchType === "series"}
-              onChange={(e) => {
-                setSearchType(e.target.value);
-                console.log(searchType);
-              }}
-            ></input>
-            <label htmlFor="typeSeries">Series</label>
-          </div>
-        </form>
-        <div>
-          <button
-            onClick={(e) => {
-              // fetch API URL plus s={search} plus y={year} plus type={type}
-              fetch(
-                "http://www.omdbapi.com/?apikey=5d6248fb&s=" +
-                  search +
-                  "&y=" +
-                  searchYear +
-                  "&type=" +
-                  searchType
-              )
-                .then((res) => {
-                  //   console.log(res);
-                  return res.json();
-                })
-                .then((res) => {
-                  //   console.log(res);
-                  setResponse(res);
-                });
-            }}
-          >
-            Search!
-          </button>
         </div>
       </aside>
       <ResultsContainer search={search.length} response={response} />
-    </>
+    </div>
   );
 };
 
